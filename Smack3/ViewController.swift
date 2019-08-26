5f9ac21e7b68fcfade59a578a666a0b4311bc535
mohammed eloksh <mmaeloksh@gmail.com>
2019-08-26 14:57:28 +0200

    ELoksh Project

---
+++ Smack3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack3
+//
+//  Created by macbookpro on 8/26/19.
+//  Copyright © 2019 macbookpro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Smack3Tests/Smack3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Smack3Tests.swift
+//  Smack3Tests
+//
+//  Created by macbookpro on 8/26/19.
+//  Copyright © 2019 macbookpro. All rights reserved.
+//
+
+import XCTest
+@testable import Smack3
+
+class Smack3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
